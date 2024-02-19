43584280d5a78c3d669dc36ecab85069773d0036
root <root@DESKTOP-9FBDPB2>
2024-02-19 19:47:49 +0800

    first commit

---
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
+use App\Filters\V1\InvoicesFilter;
+use Illuminate\Support\Arr;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // return CustomerResource::collection(Customer::all());
+        // return Customer::all();
+        $filter = new InvoicesFilter();
+        $queryItems = $filter->transform($request); // [['column', 'operator', 'value']]
+
+        if (count($queryItems) == 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoices = Invoice::where($queryItems)->paginate();
+
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoiceRequest $request)
+    {
+        //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request) {
+       $bulk = collect($request->all())->map(function($arr, $key){
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+       });
+
+       Invoice::insert($bulk->toArray());
+
+       $data = [ "message"=> "bulk invoice added successfully!"];
+       return $data;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoice $invoice)
+    {
+        // return CustomerResource::make($customer);
+        return new InvoiceResource($invoice);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::apiResource('/customers', CustomerController::class);
+    Route::apiResource('/invoices', InvoiceController::class);
+
+    // Route::post('/invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
+    Route::post('/invoices/bulk', [InvoiceController::class, 'bulkStore']);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // api/v1
+// Route::group(['prefex' => 'v2', 'namespace' => 'App\Http\Controllers\Api\V2'], function() {
+//     Route::apiResource('/customers', CustomerController::class);
+//     Route::apiResource('/invoices', InvoiceController::class);
+// });
+
